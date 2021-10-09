d260a3ff76d6753bbec35da818714f7874245042
Day4me23 <Dayneruns5k@gmail.com>
2021-10-08 21:37:03 -0500

    Revert "Merge branch 'main' of https://github.com/Zaidis/Project-Starburst into main"
    This reverts commit 61b7c3a1622553386e2ce9b52b7d6c2168225332, reversing
    changes made to a815717c59553758d31921e007bc2ba641e797cd.

--- Project Starburst/Assets/Flashlight.cs
+++ Project Starburst/Assets/Flashlight.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     Light light;
-    [SerializeField] float maxCharge, currentCharge, rechargeRate, usageSpeed, flickerEvent, lightIntensityTarget;
-    [SerializeField] Vector2 flickerSpeedBounds;
-    float flickerSpeed, lightIntensityMax;
+    [SerializeField] float maxCharge, currentCharge, rechargeRate, usageSpeed;
     [SerializeField] KeyCode rechargeButton;
     [SerializeField] PlayerMovement playerMovement;
     private void Awake()
@@ -17,9 +15,7 @@ public class Flashlight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        flickerSpeed = PickRandomFlickerSpeed();
         currentCharge = maxCharge;
-        lightIntensityMax = light.intensity;
     }
 
     // Update is called once per frame
@@ -32,13 +28,10 @@ public class Flashlight : MonoBehaviour
         {
             //Flashlight is out of charge.
             ToggleFlashlight(false);
-        }else if(light.enabled && currentCharge <= flickerEvent)
-        {
-            Flicker();
         }
         else
         {
-            Recharge();
+            light.enabled = false;
         }
     }
 
@@ -56,34 +49,8 @@ public class Flashlight : MonoBehaviour
             {
                 //If the max charge has been met, enable the light
                 currentCharge = maxCharge;
-                light.intensity = lightIntensityMax;
                 ToggleFlashlight(true);
             }
         }
-    }
-
-    void Flicker()
-    {
-        light.intensity += flickerSpeed * Time.deltaTime;
-        if (light.intensity > lightIntensityMax)
-        {
-            flickerSpeed = PickRandomFlickerSpeed();
-            light.intensity = lightIntensityMax;
-        }  
-        else if (light.intensity < lightIntensityTarget)
-        {
-            flickerSpeed = PickRandomFlickerSpeed();
-            light.intensity = lightIntensityTarget;
-        }
-    }
-
-    float PickRandomFlickerSpeed()
-    {
-        float temp = Random.Range(flickerSpeedBounds.x, flickerSpeedBounds.y);
-        if (flickerSpeed > 0)
-            temp = -temp;
-        else
-            return temp;
-        return temp;
     }
 }
