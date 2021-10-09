6964a0d38fea0bb1b750c86a0fba7a29eeeda0c1
Jake <jaklein25@gmail.com>
2021-10-08 20:51:53 -0500

    Update Test.cs

--- Project Starburst/Assets/Code/Test.cs
+++ Project Starburst/Assets/Code/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("If you can read this, tell Jake you were able to see it");
+        print("If you are able to read this, tell Jake you were able to see it");
     }
 
     // Update is called once per frame
