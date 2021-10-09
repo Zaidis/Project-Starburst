bec761a68013cfe8c3384ab43581fb25683a9018
Levon <LevonSwenson@gmail.com>
2021-10-09 15:59:25 -0500

    interaction system basics

--- Project Starburst/Assets/Code/PlayerLook.cs
+++ Project Starburst/Assets/Code/PlayerLook.cs
@@ -4,45 +4,63 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-    public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 15F;
-    public float sensitivityY = 15F;
-    public float minimumX = -360F;
-    public float maximumX = 360F;
-    public float minimumY = -60F;
-    public float maximumY = 60F;
-    float rotationY = 0F;
-    [SerializeField] Transform transform;
+   public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+   public RotationAxes axes = RotationAxes.MouseXAndY;
+   public float sensitivityX = 15F;
+   public float sensitivityY = 15F;
+   public float minimumX = -360F;
+   public float maximumX = 360F;
+   public float minimumY = -60F;
+   public float maximumY = 60F;
+   float rotationY = 0F;
+   [SerializeField] Transform transform;
 
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+   string pickupTag = "Interactable";
 
-    void Update()
-    {
-        if (axes == RotationAxes.MouseXAndY)
-        {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+   private void Start()
+   {
+      Cursor.lockState = CursorLockMode.Locked;
+      Cursor.visible = false;
+   }
 
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+   void Update()
+   {
+      if (axes == RotationAxes.MouseXAndY)
+      {
+         float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
-        else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+         rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+         rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
-            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-        }
-    }
+         transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+      }
+      else if (axes == RotationAxes.MouseX)
+      {
+         transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+      }
+      else
+      {
+         rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+         rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+         transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+      }
+
+      Transform cameraTransform = Camera.main.transform;
+      RaycastHit hit;
+      Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+
+      if (Physics.Raycast(ray, out hit))
+      {
+         if (Input.GetKeyDown(KeyCode.E))
+         {
+            if (hit.transform.CompareTag(pickupTag))
+            {
+               hit.collider.gameObject.GetComponent<IInteractable>().Interact();
+            }
+         }
+      }
+
+   }
 
 }
