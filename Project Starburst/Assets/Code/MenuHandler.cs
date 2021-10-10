60cf12b933b1fbe0ea9a6941f1bc134f73beb772
Jake <jaklein25@gmail.com>
2021-10-10 15:45:41 -0500

    Tutorial

---
+++ Project Starburst/Assets/Code/MenuHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] m_normalTextElements, m_tutorialTextElements;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToggleTutorial(false);
+    }    
+
+    public void ToggleTutorial(bool enableTutorial)
+    {
+        foreach (GameObject obj in m_normalTextElements)
+        {
+            obj.SetActive(!enableTutorial);
+        }
+
+        foreach (GameObject obj in m_tutorialTextElements)
+        {
+            obj.SetActive(enableTutorial);
+        }
+    }
+}
