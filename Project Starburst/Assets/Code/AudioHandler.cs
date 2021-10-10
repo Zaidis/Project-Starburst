b140c6875414349443105760d277e1021c054f8a
Jake <jaklein25@gmail.com>
2021-10-10 01:04:41 -0500

    Level 1 Pre Audio

---
+++ Project Starburst/Assets/Code/AudioHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHandler : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource m_audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Stop the music
+        MusicManager.instance.ToggleMusic(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_audioSource.isPlaying)
+        {
+            LoadLevel.instance.LoadNextLevel();
+        }
+    }
+}
